55dfdb6f6bd3978adbd6dac238acba839e5ead25
tuan <1936625305@qq.com>
2017-04-28 13:40:36 +0800

    好v

--- src/main/java/com/luna/test/Test.java
+++ src/main/java/com/luna/test/Test.java
@@ -3,7 +3,5 @@ package com.luna.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("000001");
-		System.out.println("999999");
-		System.out.println("ÄãºÃ²âÊÔ´úÂë");
 	}
 }
