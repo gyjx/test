a4c383844f985652ed4206280e28f155821acc40
tuan <1936625305@qq.com>
2017-04-28 16:51:04 +0800

    000

--- src/main/java/com/luna/test/Test.java
+++ src/main/java/com/luna/test/Test.java
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 package com.luna.test;
 
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("master分支");
-	}
-}
-=======
-package com.luna.test;
-
-public class Test {
-	public static void main(String[] args) {
 		System.out.println("dev分支");
 		System.out.println("test分支");
 	}
 }
->>>>>>> refs/heads/dev
